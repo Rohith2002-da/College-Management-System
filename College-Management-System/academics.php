255693f6fd27d211a1321ec659d5edc244d9b109
Rohith2002-da <85814295+Rohith2002-da@users.noreply.github.com>
2021-12-11 18:57:29 +0530

    Add files via upload

---
+++ College-Management-System/academics.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Academics</title>
+</head>
+<body>
+	<?php include('common/header.php') ?>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of Engineering and Technology</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					<h4 class="dean-message-heading mt-3">Dean's Message</h4>
+					<div class="col-lg-12 text-justify">
+						<p>
+							It is a pleasure to welcome the prospective candidates and their parents/guardians to the ICBS.
+						</p>
+						<p>
+							FOE&T has always endeavored to empower the graduates with knowledge and skills.
+							essential for current job market.
+						</p>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of Art, Social Sciences and Humanities</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					<h4 class="dean-message-heading mt-3">Dean's Message</h4>
+					<div class="col-lg-12 text-justify">
+						<p>
+							It is a pleasure to welcome the prospective candidates and their parents/guardians to the ICBS.
+You will also experience community engagement and real life learning through Rural Immersion, Urban Impact Projects and International Immersion. We have planned for alternate project learning for you until the situation improves for field visits and travel related to Rural Immersion and International Immersion.
+						</p>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of Computer Sciences</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					<h4 class="dean-message-heading mt-3">Dean's Message</h4>
+					<div class="col-lg-12 text-justify">
+						
+						<p>
+							 Campus is centrally located on the highly green canal road and can easily be approached from any corner of madurai. The infra-structure and the atmosphere combine to provide great working and studying environment. Welcome to the most fulﬁlling and learning years of your life.
+						</p>
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container-fluid">
+		<div class="row mb-5">
+			<div class="academic-area-headings">
+				<h4 class="py-4">Faculty of Business Administration</h4>
+			</div>
+			<div class="container">
+				<div class="row">
+					<h4 class="dean-message-heading mt-3">Dean's Message</h4>
+					<div class="col-lg-12 text-justify">
+						
+						
+						<p> placements learning, industry collaborations and expert speaker series will begin upon arrival and will continue throughout your PGP program.
+						</p>
+							
+					</div>	
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
